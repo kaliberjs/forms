b52f2ee1c3b043ccceda03aecb4603a488f7f4c6
EECOLOR <eecolor@qirx.org>
2019-12-21 21:36:34 +0100

    remove types (again)

--- src/types.d.ts
+++
@@ -1,6 +0,0 @@
-type FormFields = object
-type Validation = ValidationArray | ValidationFunction
-type ValidationArray = Array<ValidationFunction>
-type ValidationObject = { validation: ValidationFunction }
-type ValidationFunction = (value: any) => ValidationResult | undefined
-type ValidationResult = { id: string, params: Array<any> }
