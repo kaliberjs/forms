2098e5fe3b784b94a21b9d3c50c07e1b653ab917
EECOLOR <eecolor@qirx.org>
2019-12-26 23:53:07 +0100

    what should be present in the `Full` example?

---
+++ example/src/domain/Full.js
@@ -0,0 +1,25 @@
+export function Full() {
+  // useForm
+    // fields
+      // optional field
+      // required field
+      // custom validation
+      // validation array
+      // validation using form
+      // validation using context
+      // object field (with and without validation)
+      // array field (with and without validation)
+    // validate
+    // initial values
+    // handleSubmit
+
+  // useFieldValue (machinery/FormValues)
+
+  // useField (machinery/FormTextInput) - note to self: check if we use all state props
+  // useNumberField (machinery/FormNumberInput)
+  // useArrayField (machinery/FormArrayField)
+
+  // (machinery/FormObjectField)
+
+  // counter (to show form values are preserved)
+}
