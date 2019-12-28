3545e12b1f46f8f75892eb16b05b066f13f0f7c9
EECOLOR <eecolor@qirx.org>
2019-12-28 01:26:20 +0100

    improved validation

--- src/validation.js
+++ src/validation.js
@@ -1,12 +1,12 @@
 export const optional = null
-export const required = x => !x && message('required')
+export const required = x => !x && x !== false && x !== 0 && message('required')
 
 export const number = x => Number(x) !== x && message('number')
 export function min(min) { return x => x < min && message('min', min) }
 export function max(max) { return x => x > max && message('max', max) }
 
-export function minLength(min) { return x => x.length < min && message('minLength', min) }
-export function maxLength(max) { return x => x.length > max && message('maxLength', max) }
+export function minLength(min) { return x => x && x.length < min && message('minLength', min) }
+export function maxLength(max) { return x => x && x.length > max && message('maxLength', max) }
 
 const emailRegex = /.+@.+\..+/
 export const email = x => x && !emailRegex.test(x) && message('email')
